6a5e8758663df7c22dfb2918af449871e6c13afc
ericcarry88828 <ericcarry88828@gmail.com>
2025-01-03 08:09:31 +0800

    update

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'your_secret_key'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Nocapinmybag52014@localhost:3306/blogging'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<username>:<password>@localhost:<port>/<db_name>'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
